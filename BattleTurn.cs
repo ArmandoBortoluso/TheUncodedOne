4ab110d37871f7157ee5405720e40cbe57d87c2a
Armando B <armando.b.moura@gmail.com>
2023-07-18 08:23:00 -0300

    New BattleTurn Static Class

---
+++ BattleTurn.cs
@@ -0,0 +1,79 @@
+static class BattleTurn{
+
+    public static void humanTurn(Chara protag, List<Chara> enemies){
+
+        short movementIndex;
+        short enemyIndex;
+
+        Console.WriteLine($"It is {protag.charName}'s turn.\nYou have the following actions:");
+        protag.showMovement();
+
+        while(true){
+
+            if(Int16.TryParse(Console.ReadLine(), out movementIndex) && verifySelection(movementIndex, protag.getNumberOfMoves())){
+
+                if(verifySelection(movementIndex, protag.getNumberOfMoves())){
+                    Console.WriteLine("Please select a target:");
+                    writeCharaOnScreen(enemies);
+                    
+
+                    if(Int16.TryParse(Console.ReadLine(), out enemyIndex) && verifySelection(enemyIndex, enemies.Count)){
+                        movementIndex -= 1;
+                        enemyIndex -= 1;
+
+                        protag.action(enemies[enemyIndex], movementIndex);
+                        verifyParty(enemies);
+                        return;
+                        
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorry, I didn't get that... Could you repeat it?");
+        }
+    }
+
+    public static void cpuTurn(Chara protag, List<Chara> enemies){
+
+        protag.action(enemies[0], 0);
+        verifyParty(enemies);
+    }
+
+    //Write group's character status on the screen
+    private static void writeCharaOnScreen(List<Chara> group){
+        short i = 1;
+        foreach(Chara c in group){
+            Console.WriteLine($"{i}-{c.charName} - {c.healthPoints}/{c.totalMaxHealth}");
+            i++;
+        }
+    }
+
+    //Verify user input
+    private static Boolean verifySelection(int select, int size){
+
+        if(select <= size && select > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //Verify group consistence
+    private static void verifyParty(List<Chara> party){
+
+        List<int> indexForRemoval = new List<int>();
+
+        foreach(Chara chara in party){
+            if(chara.healthPoints == 0) indexForRemoval.Add(party.IndexOf(chara));
+        }
+
+        if(indexForRemoval.Count != 0){
+            foreach(int i in indexForRemoval){
+                party.RemoveAt(i);
+            }
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿//TODO: FIND A WAY TO GIVE PARTY'S CONTROL TO THE CPU OR THE PLAYER
-//
+﻿//
 //
 
 //Instatiate enemies and player
@@ -12,11 +11,10 @@ List<List<Chara>> enemyGroups = new List<List<Chara>>();
 //Decides if is the player's turn
 Boolean isPlayerTurn = true;
 
-//Instantiate user's options
-string? playOption;
-short movementIndex;
-short enemyIndex;
+Boolean isPlayerDead = false;
 
+//Instantiate user's player option
+string? playOption;
 
 Console.Write("Please input the player's name:");
 string? playerName = Console.ReadLine();
@@ -48,122 +46,65 @@ while(playOption != "1" && playOption != "2" && playOption != "3"){
 
 Console.WriteLine("The battle begins!, " + playerName);
 
+//The battle starts here
 foreach(List<Chara> group in enemyGroups){
+    if(isPlayerDead){
+        break;
+    }
 
     Console.WriteLine("A new enemy group appeared!!!!");
     
-
     while(true){
 
         if(isPlayerTurn){
         
             foreach(Chara Charact in playersParty){
+                if(playOption == "1" || playOption == "3"){
 
-                battleTurn(Charact, group);
+                    BattleTurn.humanTurn(Charact, group);
+
+                } else {
+
+                    BattleTurn.cpuTurn(Charact, group);
+                }
             }
-        
-    } else {
+        } else {
 
-        foreach(Chara Charact in group){
-            battleTurn(Charact, playersParty);
+            foreach(Chara Charact in group){
+                if(playOption != "3"){
+
+                    BattleTurn.cpuTurn(Charact, playersParty);
+                    
+                } else {
+
+                    BattleTurn.humanTurn(Charact, playersParty);
+                }
+            
+            }
         }
+
+        isPlayerTurn = isPlayerTurn ? false : true; 
+
+        if(group.Count == 0){
+
+            Console.WriteLine("\nYou beated this group!!\n");
+            break;
+
+        } else if(playersParty.Count == 0){
+
+            Console.WriteLine("You lost!\n");
+            isPlayerDead = true;
+            break;
+        }
+
+        Thread.Sleep(1000);
     }
-
-    isPlayerTurn = isPlayerTurn ? false : true; 
-
-    verifyParty(playersParty);
-    verifyParty(group);
-
-    if(group.Count == 0){
-
-        Console.WriteLine("\nYou beated this group!!\n");
-        break;
-
-    } else if(playersParty.Count == 0){
-
-        Console.WriteLine("You lost!\n");
-        break;
-    }
-
-    Thread.Sleep(1000);
-    }
-
 }
+
     
 if(playersParty.Count == 0){
     Console.WriteLine("You were defeated!");
 } else {
     Console.WriteLine("You won!");
-}
-
-
-//Method that controls players choices and turn result
-void battleTurn(Chara protag, List<Chara> enemies){
-
-    Console.WriteLine($"It is {protag.charName}'s turn.\nYou have the following actions:");
-    protag.showMovement();
-
-    if(playOption == "1" || playOption == "3"){
-
-        while(true){
-
-            if(Int16.TryParse(Console.ReadLine(), out movementIndex) && verifySelection(movementIndex, protag.getNumberOfMoves())){
-
-                if(verifySelection(movementIndex, protag.getNumberOfMoves())){
-                    Console.WriteLine("Please select a target:");
-                    writeCharaOnScreen(enemies);
-                    
-
-                    if(Int16.TryParse(Console.ReadLine(), out enemyIndex) && verifySelection(enemyIndex, enemies.Count)){
-                        movementIndex -= 1;
-                        enemyIndex -= 1;
-
-                        protag.action(enemies[enemyIndex], movementIndex);
-                        return;
-                        
-                    }
-                }
-            }
-
-            Console.WriteLine("Sorry, I didn't get that... Could you repeat it?");
-        }
-    }
-}
-
-//Verify group consistence
-void verifyParty(List<Chara> party){
-
-    List<int> indexForRemoval = new List<int>();
-
-    foreach(Chara chara in party){
-        if(chara.healthPoints == 0) indexForRemoval.Add(party.IndexOf(chara));
-    }
-
-    if(indexForRemoval.Count != 0){
-        foreach(int i in indexForRemoval){
-            party.RemoveAt(i);
-        }
-    }
-
-
-}
-
-//Write group's character status on the screen
-void writeCharaOnScreen(List<Chara> group){
-    short i = 1;
-    foreach(Chara c in group){
-        Console.WriteLine($"{i}-{c.charName} - {c.healthPoints}/{c.totalMaxHealth}");
-        i++;
-    }
-}
-
-//Verify user input
-Boolean verifySelection(int select, int size){
-
-    if(select <= size && select > 0){
-        return true;
-    } else {
-        return false;
-    }
 }
 
